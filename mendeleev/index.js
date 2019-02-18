38ac2c9f54a70659f6a23a1d4f75c25c76fc51e5
Bogdan <bogdya2@gmail.com>
2019-02-18 23:00:44 +0300

    add chemistry

---
+++ mendeleev/index.js
@@ -0,0 +1,12 @@
+const mendeleevTable = require('@chemistry/elements').ChemElements;
+
+const getChemistryElementBySymbol = (symbol) => {
+    let element = mendeleevTable.getBySymbol(symbol);
+    return element ? {
+        name: element.name,
+        number: element.id,
+        weight: element.mass
+    } : null;
+}
+
+module.exports = getChemistryElementBySymbol;
